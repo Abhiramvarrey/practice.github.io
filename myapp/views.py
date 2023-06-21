bd05e38b8089e0980d1757889551ce6fff63107e
Abhiramvarrey <96932995+Abhiramvarrey@users.noreply.github.com>
2023-06-21 10:24:54 +0530

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -4,7 +4,7 @@ from .forms import newuser
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'index.html')
 
 
 def signup(request):
